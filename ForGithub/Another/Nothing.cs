07e75dbcc4094bd4d80ed395126960a1068c415e
abdugofurjon05 <gofurjonshuxratov05@gmail.com>
2025-04-09 20:48:02 +0500

    add new folder

---
+++ ForGithub/Another/Nothing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ForGithub.Another
+{
+    internal class Nothing
+    {
+       public static void Another()
+       {
+            Console.WriteLine("print god damn it");
+       }
+    }
+}
