bfcffc68a9de1a753731fbd52bf32af028c6a66b
abdugofurjon05 <gofurjonshuxratov05@gmail.com>
2025-04-11 08:27:26 +0500

    added for loop

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -14,6 +14,15 @@ namespace ConsoleApp9
             Console.WriteLine("merged to main");
             Console.WriteLine("merged to main");
 
+            //added new comment
+
+            var arr = new[] { 1, 2, 3, 4, 4, 5, 6 };
+            for(int i = 0;i<arr.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
             Console.ReadKey();
         }
     }
